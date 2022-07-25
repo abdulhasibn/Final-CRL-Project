ba783392f679c33b03c92be5bd442e7f319744a6
Abdul Hasib <abdulhasibn@gmail.com>
2022-07-25 10:56:45 +0530

    Try to make the navbar responsive
    In this attempt, I added Javascript to make the navbar respond to the
    click made on the .nav_toggle. But, I was not able to do that, mainly
    because I haven't internalized the script, as I am yet to explore it.
    
    However, the site is completely responsiveness, without any glitches,
    except for the inactivity of the hamburger menu. Will try to fix it
    in the next commit.

---
+++ javascript.js
@@ -0,0 +1,5 @@
+const navToggle = document.querySelector(".nav_toggle");
+const nav = document.querySelector(".nav");
+navToggle.addEventListener("click", () => {
+    nav.classList.toggle(".nav--visible");
+})
